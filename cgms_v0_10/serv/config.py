54c5b19c9ee45287c0d56752aaebb1b86bf274c6
hmc68 <2693314544@qq.com>
2024-07-01 17:45:02 +0800

    Add files via upload

--- cgms_v0_10/serv/config.py
+++ cgms_v0_10/serv/config.py
@@ -6,7 +6,7 @@ from .jinjapage import view_page  # noqa: F401
 
 logging.basicConfig(level=logging.INFO, format = '%(asctime)s %(name)s %(levelname)s %(message)s')
 
-dblock, lifespan = create_dpool("host=localhost dbname=examdb user=examdb", min_size=4)
-
+# dblock, lifespan = create_dpool("host=localhost dbname=examdb user=examdb", min_size=4)
+dblock, lifespan = create_dpool("host=localhost dbname=newdb user=newdb options='-c client_encoding=utf8'", min_size=4)
 app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
